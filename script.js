6ad5daf29032660f2c22e1769b648e54ad904787
sinanbh <msbh1011@gmail.com>
2022-04-04 14:28:55 +0530

    portfolio upload.

---
+++ script.js
@@ -0,0 +1,11 @@
+let slideMenu = document.querySelector('.menu-slide')
+
+function openMenu()
+{
+    slideMenu.style.display = 'block';
+}
+  
+function closeMenu()
+{
+    slideMenu.style.display = 'none';
+}
