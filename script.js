754903e43089413ebbc7bf9bd72aa09f130dff0d
sinanbh <96302393+sinanbh@users.noreply.github.com>
2022-04-04 18:23:32 +0530

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let slideMenu = document.querySelector('.menu-slide')
+let navBar = document.querySelector('nav');
 
 function openMenu()
 {
     slideMenu.style.display = 'block';
+    navBar.style.display = 'static';
 }
   
 function closeMenu()
 {
     slideMenu.style.display = 'none';
+    navBar.style.display = 'fixed';
 }
